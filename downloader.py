ab38339dd5206a7cbb130f68fddbc8f0ca3248b5
pit <notthewitcher@gmail.com>
2025-02-24 21:33:27 +0530

    added railway.app fiels

--- downloader.py
+++ downloader.py
@@ -250,5 +250,5 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 10000))
+    port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port)
